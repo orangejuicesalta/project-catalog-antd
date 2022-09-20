6833e7eda6b831a7b98e9fc3eeb44057aa887b0e
Saltanat Otegenova <96764498+orangejuicesalta@users.noreply.github.com>
2022-09-20 16:37:30 +0600

    Update mainStore.js

--- src/stores/mainStore.js
+++ src/stores/mainStore.js
@@ -6,7 +6,7 @@ export const useStore = defineStore("main", () => {
   const catalogAPI = "http://10.10.1.74:80/api/v1/catalog/categories";
   const headers = {
     Authorization:
-      "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTAuMTAuMS42NTo4MDkwL2FwaS9sb2dpbiIsImlhdCI6MTY2MDU0NDkzOCwiZXhwIjoxNjYxNDA4OTM4LCJuYmYiOjE2NjA1NDQ5MzgsImp0aSI6IkFaRkVBQnY2RllLNmlNMDQiLCJzdWIiOiI4MTUiLCJwcnYiOiI2ZmFjMWQ5ZjU5OTVhMzkxODNmNmRiYTdkY2QyNDZiNGI5ZGRmMjFkIiwiaWQiOjgxNSwiZmlyc3RfbmFtZSI6Inphbmdhci5tIiwicm9sZSI6bnVsbH0.37DKmFxTmeIEa8hRn9sf3eaGHGqpCcJ0KhobD4c1Lp0",
+      "Bearer undefined",
     Accept: "application/json",
   };
 
